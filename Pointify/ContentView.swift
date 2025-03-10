02a7fd49a9f9d946629152035a1c394ef930ac3d
nuochka <anstskovalenko@gmail.com>
2025-03-11 00:07:21 +0100

    Added a welcome page

--- Pointify/ContentView.swift
+++ Pointify/ContentView.swift
@@ -9,13 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            VStack(spacing: 30){
+                Spacer()
+                
+                Image("welcome_picture")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+                    .padding()
+                
+                Text("Welcome to Pointify!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                
+                Text("Track your points, stay motivated and achieve your goals!")
+                    .font(.title3)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.gray)
+                    .padding(.horizontal, 5.0)
+                
+                Spacer()
+                
+                NavigationLink(destination: LoginView()){
+                    Text("Get Started")
+                        .fontWeight(.semibold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(Color.white)
+                        .cornerRadius(12)
+                    
+                }
+                .padding(.horizontal)
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
