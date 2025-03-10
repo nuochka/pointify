16036ea0ae577f42e67cc00499dcfd24ed14ec2f
nuochka <anstskovalenko@gmail.com>
2025-03-10 23:03:30 +0100

    Added project initialization

---
+++ Pointify/PointifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PointifyApp.swift
+//  Pointify
+//
+//  Created by nuochka on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PointifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PointifyTests/PointifyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PointifyTests.swift
+//  PointifyTests
+//
+//  Created by nuochka on 10/03/2025.
+//
+
+import Testing
+
+struct PointifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
