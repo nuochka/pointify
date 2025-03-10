e60a208ca9a288e4c8d629453e27fc30dcaa4d19
nuochka <anstskovalenko@gmail.com>
2025-03-11 00:08:00 +0100

    Added a login page template

---
+++ Pointify/LoginView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginView.swift
+//  Pointify
+//
+//  Created by nuochka on 10/03/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            Text("Login Page")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+        }
+        .navigationTitle("Login")
+    }
+}
