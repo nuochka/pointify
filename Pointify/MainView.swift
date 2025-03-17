f271e8e0dfe47664cc47c5ac3046371738b0cad9
nuochka <anstskovalenko@gmail.com>
2025-03-17 22:06:47 +0100

    Added a main page of application for authenticated users

---
+++ Pointify/MainView.swift
@@ -0,0 +1,28 @@
+//
+//  MainView.swift
+//  Pointify
+//
+//  Created by nuochka on 17/03/2025.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        NavigationStack{
+            VStack(spacing: 30){
+                Spacer()
+                
+                Text("Hello!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
