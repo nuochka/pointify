ad867fab99e299f59d801bc35870852d57835b6c
nuochka <anstskovalenko@gmail.com>
2025-04-07 09:17:48 +0200

    Added logic to handle user points adjustment when tasks or goals are marked as completed or incomplete.

--- PointifyServer/Sources/App/Controllers/UserController.swift
+++ PointifyServer/Sources/App/Controllers/UserController.swift
@@ -10,24 +10,29 @@ final class UserController: RouteCollection {
         let users = routes.grouped("users")
 
         users.get(":userId", "points") { req async throws -> UserPointsResponse in
+            guard let userId = req.parameters.get("userId", as: UUID.self),
+                let user = try await User.find(userId, on: req.db) else {
+                throw Abort(.notFound, reason: "User not found")
+            }
+            print("Points: \(user.id!): \(user.points)")
+            return UserPointsResponse(totalPoints: user.points)
+        }
+        
+        users.put(":userId", "points") { req async throws -> HTTPStatus in
             guard let userId = req.parameters.get("userId", as: UUID.self) else {
                 throw Abort(.badRequest, reason: "Invalid user ID")
             }
 
-            print("Fetching points for user with ID: \(userId)")
+            guard let user = try await User.find(userId, on: req.db) else {
+                throw Abort(.notFound, reason: "User not found")
+            }
 
-            let tasks = try await Task.query(on: req.db)
-                .filter(\.$userId == userId)
-                .filter(\.$isCompleted == true)
-                .all()
+            let updateData = try req.content.decode(UserPointsResponse.self)
+            user.points = updateData.totalPoints
+            
+            try await user.save(on: req.db)
 
-            print("Found \(tasks.count) completed tasks for user \(userId)")
-
-            let totalPoints = tasks.reduce(0) { $0 + $1.points }
-
-            print("Total points for user \(userId): \(totalPoints)")
-
-            return UserPointsResponse(totalPoints: totalPoints)
+            return .ok
         }
 
     }
